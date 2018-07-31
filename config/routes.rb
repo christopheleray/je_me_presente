b9b5b966914c031c911f026ec341ddb58385935a
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-31 19:18:10 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  root to: 'static_pages#home', as: 'home'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/about/team', to: 'static_pages#about_team', as: 'about_team'
+  get '/about/me', to: 'static_pages#about_me', as: 'about_me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
